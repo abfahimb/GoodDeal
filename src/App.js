c087b17e09a16266041d3c7aa216c808921fd772
Abu Nasim Lomani <68380516+abu-nasim-lomani@users.noreply.github.com>
2021-06-03 20:20:46 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,1 @@
-import './App.css';
-import Dashboard from './Dashboard/DashBoard/Dashboard';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router } from 'react-router-dom';
-function App() {
-  return (
-    <Router className='App'>
-      <Dashboard />
-    </Router>
-  );
-}
 
-export default App;
